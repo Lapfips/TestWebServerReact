c635b5c3291c68f0244c493f0be18244984201cd
Lapfips <fargier.ethan@gmail.com>
2025-01-23 11:30:17 +0100

    Update .css files

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="container-about">
+      <h2>About us</h2>
+      <p>
+        This react web this was created by the IPI students to allow users to
+        create custom profesionnal devis based on a csv file and the user's
+        informations
+      </p>
+    </div>
+  );
+};
+
+export default About;
